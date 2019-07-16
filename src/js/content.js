9a3ba1ca8745b9a0fdd1336df0f7afdd939c4229
Dmitriy Onischenko <coder@zeoalliance.com>
2019-07-17 01:47:36 +0300

    add style and basic ui template

--- src/js/content.js
+++ src/js/content.js
@@ -1,1 +1,17 @@
-console.log('Hello from extension');
+import '../css/content.css';
+
+const tempHtml = `
+  <section class="search">
+    <input class="input" type="text" placeholder="Search" />
+    <button class="next">‹</button>
+    <button class="prev">›</button>
+  </section>
+`;
+
+const renderSearch = (html) => {
+  const searchWrapper = document.createElement('div');
+  searchWrapper.innerHTML = html;
+  document.body.appendChild(searchWrapper);
+};
+
+renderSearch(tempHtml);
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,18 +1,41 @@
-import path from 'path'
-import CopyPlugin from 'copy-webpack-plugin'
+import path from 'path';
+import CopyPlugin from 'copy-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
 const config = {
   mode: 'development',
-  context: path.join(__dirname, 'src'),
+  context: path.join(__dirname, 'src', 'js'),
   entry: {
-    background: './js/background.js',
-    content: './js/content.js',
-    options: './js/options.js',
-    popup: './js/popup.js'
+    background: './background.js',
+    content: './content.js',
+    options: './options.js',
+    popup: './popup.js'
   },
   output: {
     path: path.join(__dirname, 'build'),
-    filename: '[name].bundle.js'
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
+      }, {
+        test: /\.html$/,
+        loader: 'html-loader',
+        exclude: /node_modules/
+      }, {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+        exclude: /node_modules/
+      }
+    ]
   },
   plugins: [
     new CopyPlugin([
@@ -21,8 +44,24 @@ const config = {
         to: './manifest.json',
         context: path.join(__dirname)
       }
-    ])
+    ]),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'background.html'),
+      filename: 'background.html',
+      chunks: ['background']
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'options.html'),
+      filename: 'options.html',
+      chunks: ['options']
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'popup.html'),
+      filename: 'popup.html',
+      chunks: ['popup']
+    }),
+    new CleanWebpackPlugin()
   ]
-}
+};
 
-export default config
+export default config;
